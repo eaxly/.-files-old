83d6cd0284760555a01af9511f89f0e19f76b64d
Samuel <axolotl@pop-os.localdomain>
2021-03-13 12:24:24 +0100

    Trying to fix this issue

--- remote-install.sh
+++ remote-install.sh
@@ -2,18 +2,36 @@
 
 DOTFILES="$HOME/.dotfiles"
 GITHUB_REPO_URL="https://github.com/ExtinctAxolotl/dotfiles"
-is_installed() {
-command -v $1 > /dev/null 2>&1
+install_app() {
+  info "Trying to find ${1}"
+	if ! is_installed_pkg $1; then
+		read -p "Do you wan't to install $1? [y/N] " -n 1 answer
+		if [ ${answer} == "y" ]; then
+			info "Installing ${1}..."
+			sudo apt install $1
+		else
+			info "${1} Will not be installed!\nSkipping...\n"
+			sleep 1
+		fi
+	else
+		success "${1} is already installed!\nSkipping...\n"
+		sleep 1
+	fi
 }
-install_dotfiles() {
-  info "Trying to detect if dotfiles are installed..."
 
-  if [ ! -d $DOTFILES ]; then
-    info "Dotfiles don't exist..."
-    if is_installed git; then
+is_installed_pkg() {
+	command -v $1 >/dev/null 2>&1
+}
+
+install_dotfiles() {
+	info "Trying to detect if dotfiles are installed..."
+
+	if [ ! -d $DOTFILES ]; then
+		info "Dotfiles don't exist..."
+		if is_installed_pkg git; then
 			read -p "Do you wan't to install dotfiles? [y/N]" -n 1 answer
 
-			if [ ${answer} != "y" ];then 
+			if [ ${answer} != "y" ]; then
 				exit 1
 			fi
 
@@ -22,8 +40,8 @@ install_dotfiles() {
 		else
 			info "Seems like git isn't installed!"
 			install_app git
-    fi
-  fi
+		fi
+	fi
 }
 
 install_dotfiles
